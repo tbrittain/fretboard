2c3f6451d2d363159b6265970db1673674da48ec
Trey Brittain <treybrittain@gmail.com>
2026-01-01 20:58:48 -0600

    feat: add Button and Header components with stories and styles

---
+++ src/components/ChordInspector.stories.ts
@@ -0,0 +1,17 @@
+﻿import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import ChordInspector from "@/components/ChordInspector.tsx";
+
+const meta = {
+    title: 'Chord Inspector',
+    component: ChordInspector,
+    parameters: {
+        // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+        layout: 'fullscreen',
+    },
+} satisfies Meta<typeof ChordInspector>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
